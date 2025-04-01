e5d497f6e3a8841ed6a188742201a4f2d3e0b076
Sebastian Bustamante <149102302+SebastianBust@users.noreply.github.com>
2025-04-01 15:04:34 -0500

    Update source.py

--- source.py
+++ source.py
@@ -155,8 +155,7 @@ if not csv_files:
 # For each CSV file, read into a single dataframe
 scans_dfs = []
 for csv in csv_files:
-    df = pd.read_csv(csv, index_col=None, header=0)
-    scans_dfs.append(df)
+    scans_dfs.append(pd.read_csv(csv, index_col=None, header=0))
 aggregate_scans_df = pd.concat(scans_dfs, ignore_index=True)
 
 print(f"Total files aggregated to dataframe: {len(csv_files)}")
@@ -170,7 +169,7 @@ print(aggregate_scans_df)
 ##########################################################################################################
 
 # Saving the DataFrame as a CSV file 
-scans_csv_data = scans_df.to_csv('aggregate_reports.csv', index = False) 
+scans_csv_data = aggregate_scans_df.to_csv('aggregate_reports.csv', index = False) 
     
 # For readability
 print()
