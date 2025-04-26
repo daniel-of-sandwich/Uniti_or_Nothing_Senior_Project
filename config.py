af7e1cfcf95ecf20d0b4c9d453b48bea87e9779d
daniel-of-sandwich <123020027+daniel-of-sandwich@users.noreply.github.com>
2025-04-25 23:01:49 -0500

    v0.6
    - Replaced CSV aggregation with a SQLite database.
    - Scan reports are downloaded as CSVs, imported into pandas DataFrames and cleaned of useless information, then inserted into database tables (Vulnerability, Scan, Folder).
    - Folder table includes "last_modification_date" feature which can be joined with the Vulnerability table by a shared folder id feature to get vulnerability discovery dates in Grafana.
    - Grafana dashboard JSON included.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # NOTE -- Comes with hardcoded values. This would normally be blank / have dummy values for the user to put in
 
-# Make sure the Tenable Nessus port (8834 by default) is open on the Nessus machine's firewall
+# Make sure the Tenable Nessus port (8834 by default) is open or allowed on the host machine's firewall. Be wary of dynamic ip assignment -- this will likely change if not using a static ip
 # Should be of the form https://<ip_address>:<Nessus_port>
 NESSUS_URL = r'https://192.168.50.188:8834'
 
@@ -8,5 +8,11 @@ NESSUS_URL = r'https://192.168.50.188:8834'
 ACCESS_KEY = '8c04ecd4caca9fc56e0fa18e999712f6cb76396f0c033aebb7e51b7f548fc1b2'
 SECRET_KEY = '29a489ce023cd2422354a369055ba4aa1a4f7365c4c74733e00b59c78ee079dc'
 
-# List of folders whose scan reports should be aggregated. If blank, uses all folders except 'Trash'
+# List of folders whose scan reports should be aggregated. If empty, uses all folders except 'Trash'
 FOLDERS = []
+
+# List of features to keep from the exported scan report CSVs. If empty, keeps all features
+KEEP_FEATURES = ['CVE', 'CVSS v2.0 Base Score', 'Risk', 'Host', 'Protocol', 'Port', 'Name']
+# Available features using default Tenable Nessus CSV export settings:
+# 'Plugin ID', 'CVE', 'CVSS v2.0 Base Score', 'Risk', 'Host', 'Protocol', 'Port', 'Name', 'Synopsis', 'Description', 'Solution', 'See Also', 'Plugin Output'
+# IMPORTANT -- If changed, the sqlite3 'Vulnerability' table features will need to be altered
--- source.py
+++ source.py
@@ -1,25 +1,31 @@
-# v0.5
+# v0.6
 
 # Uniti or Nothing team
 # CIS-497-101 Spring 2025
 # University of South Alabama
 
 # Resources---
-# Nessus API: https://<nessus_server_ip>:<port>/api
+# Nessus API: https://<nessus_server_ip>:<nessus_port>/api
 # Python requests library, API: https://pypi.org/project/requests, https://requests.readthedocs.io/en/latest/api
 # Python os library: https://docs.python.org/3/library/os.html
-# akinnane cyber-security-nessus GitHub repo: https://github.com/alphagov-mirror/cyber-security-nessus
+# akinnane cyber-security-nessus GitHub repo: https://github.com/alphagov-mirror/cyber-security-nessus for get() and post() code
+# Python sqlite3 library: https://docs.python.org/3/library/sqlite3.html
+# Python pandas library to_csv function: https://pandas.pydata.org/docs/user_guide/10min.html#csv
 
-# get(), post(), put() code taken and modified from akinnane cyber-security-nessus repo
+# Tested and developed on Debian GNU/Linux 12 (bookworm) virtual machine running Python 3.11.2
 
-# Tested on an Oracle VM VirtualBox Debian 12.9.0 virtual machine
+######## Setup 1/2 ########
 
 # Imports
 import config # Local file config.py
 import requests
 from requests.packages import urllib3
-import os
+from os import path, mkdir, remove
 import csv
+import pandas as pd
+import sqlite3
+from datetime import datetime # datetime class from datetime package
+from shutil import copy2
 
 # Get Nessus URL and API keys from config file
 NESSUS_URL = config.NESSUS_URL
@@ -29,13 +35,20 @@ SECRET_KEY = config.SECRET_KEY
 # X-ApiKeys HTTP header. This is in place of a username-password login
 API_KEYS = {'X-ApiKeys': f'accessKey={ACCESS_KEY}; secretKey={SECRET_KEY};'}
 
-# Suppress certificate verificaion warning when connecting to Nessus server
+# Suppress certificate verificaion warning when connecting to Nessus server (not required)
 urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
 
 # Get folder list from config file
 FOLDERS = config.FOLDERS
 
-######### HTTP request functions #########
+# Get features to keep from config file
+KEEP_FEATURES = config.KEEP_FEATURES
+
+# Filenames of sqlite3 databases holding working and current vulnerability data
+WORKING_DB = 'working.db'
+DISPLAY_DB = 'display.db'
+
+######## Functions ########
 
 # GET request to Nessus
 def get(path, text=False):
@@ -61,128 +74,242 @@ def post(path, payload, headers=None):
         verify=False
     ).json()
 
-######### Download vulnerability scan reports #########
+# Insert data into sqlite3 database tables. *_data arguments are lists of lists, or None to skip
+def insert_data(database_filename, folder_data, scan_data, vulnerability_data):
+    db = None
+    try:
+        # Connect to sqlite3 database
+        db = sqlite3.connect(database_filename)
+        cursor = db.cursor()
+        
+        # Insert data into Folder table
+        if folder_data: # If not None
+            cursor.executemany("INSERT INTO Folder (id, name, type) VALUES (?, ?, ?)", folder_data)
+            db.commit()
+        
+        # Insert data into Scan table
+        if scan_data:
+            cursor.executemany("INSERT INTO Scan (id, name, scan_type, last_modification_date, folder_id) VALUES (?, ?, ?, ?, ?)", scan_data)
+            db.commit()
+        
+        # Insert data into Vulnerability table
+        if vulnerability_data:
+            cursor.executemany("INSERT INTO Vulnerability (CVE, CVSS_v2_0_Base_Score, Risk, Host, Protocol, Port, Name, new_vuln, scan_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", vulnerability_data)
+            db.commit()
+        
+    except sqlite3.Error:
+        # Rollback changes if there is an error
+        if db:
+            db.rollback()
+    
+    finally:
+        # Close the database connection
+        if db:
+            db.close()
+
+######## Setup 2/2 ########
+
+# If reports directory does not exist, create directory
+reports_path = r'./reports'
+if not path.isdir(reports_path):
+    mkdir(reports_path)
+
+# If WORKING_DB sqlite3 database does not exist, create database and tables
+db = None
+try:
+    # Create and connect to sqlite3 database
+    db = sqlite3.connect(WORKING_DB)
+    cursor = db.cursor()
+
+    # Create Folder table
+    create_folder_table_sql = """
+        CREATE TABLE Folder (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        [type] TEXT NOT NULL
+        );
+    """
+    cursor.execute(create_folder_table_sql)
+
+    # Create Scan table
+    create_scan_table_sql = """
+        CREATE TABLE Scan (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            scan_type TEXT NOT NULL,
+            last_modification_date DATETIME NOT NULL,
+            folder_id INTEGER NOT NULL,
+            FOREIGN KEY (folder_id) REFERENCES Folder(id)
+        );
+    """
+    cursor.execute(create_scan_table_sql)
+
+    # Commit changes
+    db.commit()
+    
+    # Create Vulnerability table
+    create_vulnerability_table_sql = """
+        CREATE TABLE Vulnerability (
+            CVE TEXT,
+            CVSS_v2_0_Base_Score REAL,
+            Risk TEXT,
+            [Host] TEXT,
+            Protocol TEXT,
+            Port INTEGER,
+            Name TEXT,
+            new_vuln INTEGER NOT NULL,
+            scan_id INTEGER NOT NULL,
+            FOREIGN KEY (scan_id) REFERENCES Scan(id)
+        );
+    """
+    cursor.execute(create_vulnerability_table_sql)
+
+    # Commit changes
+    db.commit()
+
+except sqlite3.Error:
+    # Rollback changes if there is an error
+    if db:
+        db.rollback()
+
+finally:
+    # Close the database connection
+    if db:
+        db.close()
+
+######## Find vulnerability scans in selected folders ########
 
 # For readability
 print()
 
 # Get an array of folder names to export scan reports of
-r_folders_scans = get(r'/scans')
+r_scans = get(r'/scans') # Contains scan and folder information
 folder_names = []
 # If there are no user-defined folders, use all folder names except 'Trash'
 if (len(FOLDERS) == 0):
     # Get an array of all folder names
-    for folder in r_folders_scans['folders']:
+    for folder in r_scans['folders']:
         folder_names.append(folder['name'])
 
     # Remove 'Trash' from folder name list
     if 'Trash' in folder_names:
         folder_names.remove('Trash')
+        
 # If there are, use those
 else:
     folder_names = FOLDERS
 
 # Get id of each selected folder
 folder_ids = []
-for folder in r_folders_scans['folders']:
+for folder in r_scans['folders']:
     if folder['name'] in folder_names:
         folder_ids.append(folder['id'])
 
 # Get id of each scan where scan_type is vuln in selected folders
 scan_ids = []
-for scan in r_folders_scans['scans']:
+for scan in r_scans['scans']:
     if scan['folder_id'] in folder_ids:
         if scan['scan_type'] == 'vuln':
             scan_ids.append(scan['id'])
 
-# FIXME delete -- Print names of selected folders
-print('FIXME delete -- Selected folders:')
-for folder in r_folders_scans['folders']:
-    if folder['id'] in folder_ids:
-        print('\t{}:{}'.format(folder['id'], folder['name']))
-print()
-
-# FIXME delete -- Print names of "vuln" scans
-print('FIXME delete -- Scans of scan_type \"vuln\":')
-for scan in r_folders_scans['scans']:
-    if scan['id'] in scan_ids:
-        print('\t{}:{}'.format(scan['id'], scan['name']))
-print()
+######## Prepare vulnerability scan report downloads ########
 
 # Request a download token for each selected scan
-template_id = 197 # CSV template
-token_id_pairs = {}
+template_id = 197 # Not sure what this does, but it's what the scans have on the API
+token_to_scan_id = {}
+good_scan_ids = [] # scan_ids that have tokens
 for scan_id in scan_ids:
-    r_token = post(path=f'/scans/{scan_id}/export', payload={'format': 'csv', 'template_id': template_id})
+    r_export = post(path=f'/scans/{scan_id}/export', payload={'format': 'csv', 'template_id': template_id})
     token = None
     try:
         # If a token exists for the scan_id
-        token = r_token['token']
+        token = r_export['token']
     except:
         # If it doesn't for whatever reason, keep token as None
         token = None
     if token: # If not None
-        token_id_pairs[token] = scan_id
+        token_to_scan_id[token] = scan_id
+        good_scan_ids.append(scan_id)
 
-# Download the selected scans to destination_path. If latter does not exist, create directory
-destination_path = r'./reports'
-os.makedirs(destination_path, exist_ok=True)
-for token in token_id_pairs:
+######## Download scan reports, prepare Vulnerability table data ########
+
+# For every scan report able to be downloaded
+vulnerability_df = pd.DataFrame()
+for token in token_to_scan_id:
+    # Get scan_id, create filename and file path for the current token
+    scan_id = token_to_scan_id[token]
+    filename = f'{scan_id}.csv'
+    file_path = path.join(reports_path, filename)
+    
+    # Download the selected scans to reports_path
     r_download = get(path=f'/tokens/{token}/download', text=True)
-    filename = f'{token_id_pairs[token]}.csv' # Filename is scan_id
-    file_path = os.path.join(destination_path, filename)
     with open(file_path, 'w') as file:
         file.write(r_download) # Overwrites if <token_id>.csv already exists
-    print(f'File written at {file_path}') # FIXME -- delete
-
-######## Aggregate reports into a single CSV file ########
-
-# Specify the directory containing your CSV files
-input_directory = r'./reports'
-
-# Find all CSV files in the specified directory
-filenames = os.listdir(input_directory)
-csv_files = []
-for filename in filenames:
-    csv_files.append(input_directory + '/' + filename)
-
-# Check if any CSV files were found
-if not csv_files:
-    print(f"No CSV files found in directory: {input_directory}")
-    exit()
-
-# Open the output file
-agg_filename = 'aggregated_reports.csv'
-with open(agg_filename, 'w', newline='', encoding='utf-8') as outfile:
-    # Flag to write headers only once
-    headers_written = False
-
-    # Iterate through each CSV file
-    for csv_file in csv_files:
-        try:
-            with open(csv_file, 'r', encoding='utf-8') as infile:
-                reader = csv.reader(infile)
-                # Write headers only for the first file
-                if not headers_written:
-                    headers = next(reader)
-                    csv.writer(outfile).writerow(headers)
-                    headers_written = True
-                else:
-                    # Skip headers for subsequent files
-                    next(reader)
-                # Write data rows
-                for row in reader:
-                    csv.writer(outfile).writerow(row)
-
-        except Exception as e:
-            print(f"Error reading file {os.path.basename(csv_file)}: {e}")
     
-    # If there are no CSV files (no scans, maybe)
-    if not csv_files:
-        print(f"No CSV files found in directory {input_directory}. Is ./reports directory empty?")
+    # Import the downloaded CSV into a DataFrame
+    df = pd.read_csv(file_path)
     
-print(f"\nFile written at ./{agg_filename}")
-print(f"Total files aggregated: {len(csv_files)}")
+    # Remove unwanted features
+    keep_features = [df_feature for df_feature in KEEP_FEATURES if df_feature in df.columns]
+    df = df[keep_features]
+    
+    # Remove info rows (non-vulnerability)
+    df = df[df['Risk'] != 'None']
+    
+    # Append and populate new_vuln feature, will update later
+    df['new_vuln'] = 0
+    
+    # Append and populate scan_id feature
+    df['scan_id'] = scan_id
+    
+    # Concat preprocessed DataFrame to vulnerability_df
+    if vulnerability_df.empty:
+        vulnerability_df = df
+    else:
+        vulnerability_df = pd.concat([vulnerability_df, df], axis=0)
+
+######## Prepare Scan table data ########
+
+# Scan table features
+features = ['id', 'name', 'scan_type', 'last_modification_date', 'folder_id']
+scan_df = pd.DataFrame(columns=features)
+
+# For every scan
+for scan in r_scans['scans']:
+    # If the scan was downloaded
+    if scan['id'] in good_scan_ids:
+        # Get scan values
+        values = [scan['id'], scan['name'], scan['scan_type'], scan['last_modification_date'], scan['folder_id']]
+        
+        # Format features and values into a DataFrame
+        new_row = pd.DataFrame([dict(zip(features, values))])
+        scan_df = pd.concat([scan_df, new_row], axis=0, ignore_index=True)
+
+######## Prepare Folder table data ########
+
+# Folder table features
+features = ['id', 'name', 'type']
+folder_df = pd.DataFrame(columns=features)
+
+for folder in r_scans['folders']:
+    # Get folder values
+    values = [folder['id'], folder['name'], folder['type']]
+    
+    # Format features and values into a DataFrame
+    new_row = pd.DataFrame([dict(zip(features, values))])
+    folder_df = pd.concat([folder_df, new_row], axis=0, ignore_index=True)
+
+######## Insert Folder, Scan, Vulnerability data into sqlite3 database ########
+
+insert_data(WORKING_DB, folder_df.values.tolist(), scan_df.values.tolist(), vulnerability_df.values.tolist())
+
+######## Replace DISPLAY_DB with WORKING_DB ########
+
+try:
+    copy2(WORKING_DB, DISPLAY_DB)
+    remove(WORKING_DB)
+except:
+    None
 
 # For readability
 print()
